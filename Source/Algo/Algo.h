59f0a40d1fc52bdc23cb594bf90e08972e7ba19a
ArsenyAtman <74785269+ArsenyAtman@users.noreply.github.com>
2022-03-22 21:34:30 +0300

    Fix compilation errors

--- Source/Algo/Algo.h
+++ Source/Algo/Algo.h
@@ -26,16 +26,16 @@ public:
 	static int BinarySearch(const TArray<T>& SortedArray, const T& ElementToFind);
 
 	template<class T>
-	static void QuickSort(const TArray<T>& Array, ESortType SortType);
+	static void QuickSort(TArray<T>& Array, ESortType SortType);
 
 	template<class T>
-	static void MergeSort(const TList<T>& List, ESortType SortType);
+	static void MergeSort(TList<T>& List, ESortType SortType);
 
 	template<class T>
-	static void Reverse(const TArray<T>& Array);
+	static void Reverse(TArray<T>& Array);
 
 	template<class T>
-	static void Reverse(const TList<T>& List);
+	static void Reverse(TList<T>& List);
 
 	static bool IsEven(int Number);
 };
@@ -51,7 +51,7 @@ void LAlgo::Swap(T& Value1, T& Value2)
 template<class T>
 int LAlgo::LinearSearch(const IIterable<T>& Iterable, const T& ElementToFind)
 {
-	IIterator<T>* Iterator = Iterable.GetConstIterator();
+	IIteratorConst<T>* Iterator = Iterable.GetConstIterator();
 
 	int i = 0;
 	while (Iterator->HasMore())
@@ -74,23 +74,23 @@ int LAlgo::LinearSearch(const IIterable<T>& Iterable, const T& ElementToFind)
 template<class T>
 int LAlgo::BinarySearch(const TArray<T>& SortedArray, const T& ElementToFind)
 {
-
+	return -1;
 }
 
 template<class T>
-void LAlgo::QuickSort(const TArray<T>& Array, ESortType SortType)
+void LAlgo::QuickSort(TArray<T>& Array, ESortType SortType)
 {
 
 }
 
 template<class T>
-void LAlgo::MergeSort(const TList<T>& List, ESortType SortType)
+void LAlgo::MergeSort(TList<T>& List, ESortType SortType)
 {
 
 }
 
 template<class T>
-void LAlgo::Reverse(const TArray<T>& Array)
+void LAlgo::Reverse(TArray<T>& Array)
 {
 	int ForwardElement = 0;
 	int BackwardElement = Array.GetLength() - 1;
@@ -105,14 +105,14 @@ void LAlgo::Reverse(const TArray<T>& Array)
 }
 
 template<class T>
-void LAlgo::Reverse(const TList<T>& List)
+void LAlgo::Reverse(TList<T>& List)
 {
 	TListElement<T>* ForwardElement = List.GetFirstElement();
 	TListElement<T>* BackwardElement = List.GetLastElement();
 
 	while (ForwardElement != BackwardElement || ForwardElement->GetNext() != BackwardElement)
 	{
-		LAlgo::Swap(ForwardElement->GetValue(), BackwardElement->GetValue);
+		LAlgo::Swap(ForwardElement->GetValue(), BackwardElement->GetValue());
 
 		ForwardElement = ForwardElement->GetNext();
 		BackwardElement = BackwardElement->GetPrev();
