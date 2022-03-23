a1346a270426f0c8dd6906face1f21eda82ccf52
ArsenyAtman <74785269+ArsenyAtman@users.noreply.github.com>
2022-03-23 14:59:35 +0300

    LAlgo::QuickSort

--- Source/Algo/Algo.cpp
+++ Source/Algo/Algo.cpp
@@ -4,3 +4,13 @@ bool LAlgo::IsEven(int Number)
 {
 	return Number % 2;
 }
+
+bool LAlgo::IsLess(const int Value1, const int Value2)
+{
+	return Value1 < Value2;
+}
+
+bool LAlgo::IsMore(const int Value1, const int Value2)
+{
+	return Value1 > Value2;
+}
--- Source/Algo/Algo.h
+++ Source/Algo/Algo.h
@@ -38,6 +38,19 @@ public:
 	static void Reverse(TList<T>& List);
 
 	static bool IsEven(int Number);
+
+protected:
+
+	using DSortComparsion = bool(*)(int, int);
+
+	template<class T>
+	static void QuickSortAlgo(TArray<T>& Array, int FirstIndex, int LastIndex, DSortComparsion LeftComparsion, DSortComparsion RightComparsion);
+
+	template<class T>
+	static int Partition(TArray<T>& Array, int FirstIndex, int LastIndex, DSortComparsion LeftComparsion, DSortComparsion RightComparsion);
+
+	static bool IsLess(const int Value1, const int Value2);
+	static bool IsMore(const int Value1, const int Value2);
 };
 
 template<class T>
@@ -108,7 +121,17 @@ int LAlgo::BinarySearch(const TArray<T>& SortedArray, const T& ElementToFind)
 template<class T>
 void LAlgo::QuickSort(TArray<T>& Array, ESortType SortType)
 {
+	switch (SortType)
+	{
+	case ESortType::Ascending:
+		LAlgo::QuickSortAlgo(Array, 0, Array.GetLength() - 1, &LAlgo::IsLess, &LAlgo::IsMore);
+		break;
 
+	case ESortType::Descending:
+		LAlgo::QuickSortAlgo(Array, 0, Array.GetLength() - 1, &LAlgo::IsMore, &LAlgo::IsLess);
+		break;
+	}
+	
 }
 
 template<class T>
@@ -144,5 +167,40 @@ void LAlgo::Reverse(TList<T>& List)
 
 		ForwardElement = ForwardElement->GetNext();
 		BackwardElement = BackwardElement->GetPrev();
+	}
+}
+
+template<class T>
+void LAlgo::QuickSortAlgo(TArray<T>& Array, int FirstIndex, int LastIndex, DSortComparsion LeftComparsion, DSortComparsion RightComparsion)
+{
+	if (FirstIndex < LastIndex)
+	{
+		int PartitionIndex = LAlgo::Partition(Array, FirstIndex, LastIndex, LeftComparsion, RightComparsion);
+		LAlgo::QuickSortAlgo(Array, FirstIndex, PartitionIndex - 1, LeftComparsion, RightComparsion);
+		LAlgo::QuickSortAlgo(Array, PartitionIndex + 1, LastIndex, LeftComparsion, RightComparsion);
+	}
+}
+
+template<class T>
+int LAlgo::Partition(TArray<T>& Array, int FirstIndex, int LastIndex, DSortComparsion LeftComparsion, DSortComparsion RightComparsion)
+{
+	int Pivot = Array[(FirstIndex + LastIndex) / 2];
+	while (true)
+	{
+		while (LeftComparsion(Array[FirstIndex], Pivot))
+		{
+			++FirstIndex;
+		}
+		while (RightComparsion(Array[LastIndex], Pivot))
+		{
+			--LastIndex;
+		}
+
+		if (FirstIndex >= LastIndex)
+		{
+			return LastIndex;
+		}
+
+		LAlgo::Swap(Array[FirstIndex], Array[LastIndex]);
 	}
 }
